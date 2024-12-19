ca882b03f683704cbe3dd7d2e4f40a3c7b2accdc
yoshihiko takashima <yoshihiko.takashima@realize-inc.co.jp>
2024-12-19 13:39:48 +0900

    motionデータを更新

--- Assets/Script/MainController.cs
+++ Assets/Script/MainController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11d1aa85f17091d0cd3d7c5918e0f9cda7ad4adb4546ed90fd857b46b6a5a1f5
+oid sha256:7e3326b1d26b38dddb80295cddafc69a6ec6f1338af87262e6385c5184928c3f
 size 14097
